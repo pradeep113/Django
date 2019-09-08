d318b1d50b3d707beb1f6a4375bb3033b50cc8b2
pradeep113 <pradeep.prdp113@gmail.com>
2019-09-09 00:12:20 +0530

    Home page created

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def homePageView(request):
-	return HttpResponse("Hello World")
+class HomePageView(TemplateView):
+	template_name = 'home.html'
